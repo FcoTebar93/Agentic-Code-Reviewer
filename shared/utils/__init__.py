839e4a589d5d7bd47b370a82847a794a8e620531
FcoTebar93 <fcotebar93@gmail.com>
2026-03-02 08:49:59 +0100

    feat(utils): Add build_short_term_memory_window to utils module exports

--- shared/utils/__init__.py
+++ shared/utils/__init__.py
@@ -1,3 +1,4 @@
 from shared.utils.rabbitmq import EventBus
+from shared.utils.memory_window import build_short_term_memory_window
 
-__all__ = ["EventBus"]
+__all__ = ["EventBus", "build_short_term_memory_window"]
